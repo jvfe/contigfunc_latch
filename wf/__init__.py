ddf97cc4b926a20f91c089acffd85fd8313884ab
jvfe <jvfecav@gmail.com>
2022-07-22 14:29:34 -0300

    docs: Add long description to workflow

--- wf/__init__.py
+++ wf/__init__.py
@@ -16,6 +16,35 @@ def contigfunc(
     sample_name: str = "contigfunc_sample",
     fargene_hmm_model: fARGeneModel = fARGeneModel.class_a,
 ) -> List[Union[LatchFile, LatchDir]]:
+    """A pipeline to screen MAGs for functional components
+
+    ContigFunc
+    ----------
+
+    ContigFunc is a workflow for screening and prediction of functional
+    components from metagenomically assembled genomes (MAGs). It's composed of:
+
+    - [Macrel](https://github.com/BigDataBiology/macrel) for predicting Antimicrobial Peptide (AMP)-like sequences
+    - [fARGene](https://github.com/fannyhb/fargene) for identifying Antimicrobial Resistance Genes (ARGs)
+    - [Gecco](https://github.com/zellerlab/GECCO) for predicting biosynthetic gene clusters (BCGs)
+
+    ### References
+
+    Santos-Júnior CD, Pan S, Zhao X, Coelho LP. 2020.
+    Macrel: antimicrobial peptide screening in genomes and metagenomes.
+    PeerJ 8:e10555. DOI: 10.7717/peerj.10555
+
+    Berglund, F., Österlund, T., Boulund, F., Marathe, N. P.,
+    Larsson, D. J., & Kristiansson, E. (2019).
+    Identification and reconstruction of novel antibiotic resistance genes
+    from metagenomes. Microbiome, 7(1), 52.
+
+    Accurate de novo identification of biosynthetic gene clusters with GECCO.
+    Laura M Carroll, Martin Larralde, Jonas Simon Fleck, Ruby Ponnudurai,
+    Alessio Milanese, Elisa Cappio Barazzone, Georg Zeller.
+    bioRxiv 2021.05.03.442509; doi:10.1101/2021.05.03.442509
+
+    """
     macrel_results = macrel(contigs=contigs, sample_name=sample_name)
     fargene_results = fargene(
         contigs=contigs, sample_name=sample_name, hmm_model=fargene_hmm_model
